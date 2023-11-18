3d3b5025464f7e1b13d3ab461ae854dcdba8cbe8
yahyaouirim <145239153+yahyaouirim@users.noreply.github.com>
2023-11-18 05:30:46 +0100

    update counter core

--- flask/fundamentals/Counter_core/server.py
+++ flask/fundamentals/Counter_core/server.py
@@ -11,6 +11,14 @@ def home():
         session['count']+=1
 
     return render_template("index.html")
+@app.route('/two')
+def two_visitors():
+    if "count" not in session:
+        session['count']=0
+    else:
+        session['count']+=1
+    return redirect("/")
+
 @app.route('/clear')
 def reset():
     session.clear()
